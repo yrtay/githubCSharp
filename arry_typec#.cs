47a264c32b85b4e54faefcc504f68645c9bb5ef1
yrtay <yuanrentay365@gmail.com>
2023-03-18 15:00:33 +0800

    arrayC#

---
+++ arry_typec#.cs
@@ -0,0 +1,18 @@
+﻿int[] price = { 20, 10, 50 };
+string[] goods = new string[5];
+goods[0] = "apple";
+goods[1] = "book";
+goods[2] = "cable";
+goods[3] = "deck";
+goods[4] = "eraser";
+
+System.Console.WriteLine(price[0]);
+System.Console.WriteLine(price[1]);
+System.Console.WriteLine(price[2]);
+
+System.Console.WriteLine(goods[0]);
+System.Console.WriteLine(goods[1]);
+System.Console.WriteLine(goods[2]);
+System.Console.WriteLine(goods[3]);
+System.Console.WriteLine(goods[4]);
+System.Console.WriteLine(goods[5]);
