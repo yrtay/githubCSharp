7f6816b31b27c3aee3093905b4d5db8d208e9ebc
yrtay <yuanrentay365@gmail.com>
2023-03-20 11:29:55 +0800

    minigame1

---
+++ minigame1.cs
@@ -0,0 +1,17 @@
+﻿var rand = new Random();
+int num = rand.Next(100);
+/*
+這行打開可以看見隨機什麼數字
+System.Console.WriteLine(num);
+*/
+int rnum;
+do
+{
+    System.Console.WriteLine("系統已經決定好了一個數字，你可以開始猜了（*整數100以內）");
+    rnum = System.Convert.ToInt32(System.Console.ReadLine());
+}
+while (rnum != num);
+if(rnum == num)
+{
+    System.Console.WriteLine("恭喜猜對了");
+}
