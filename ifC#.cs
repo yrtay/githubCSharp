a0af0c452eff7a380d6ab87d9e4c3a76060457ed
yrtay <yuanrentay365@gmail.com>
2023-03-20 10:49:44 +0800

    ifC#

---
+++ ifC#.cs
@@ -0,0 +1,46 @@
+﻿bool man = true;
+if (man)
+{
+    System.Console.WriteLine("man");
+}
+bool woman = true;
+
+if (woman)
+{
+    System.Console.WriteLine("woman");
+}
+
+if (!woman || man)
+{
+    System.Console.WriteLine("man");
+}
+
+if ( woman || !man)
+{
+    System.Console.WriteLine("woman");
+}
+
+if (!woman || !man)
+{
+    System.Console.WriteLine("woman");
+}
+else
+{
+    System.Console.WriteLine("neutral");
+}
+
+/*
+string name  = "Bob";
+if ( name == "Bob" && man )
+{
+    System.Console.WriteLine("這是男生Bob");
+}
+else if ( name =="Bob" && woman)
+{
+    System.Console.WriteLine("這是女生Bob");
+}
+else
+{
+    System.Console.WriteLine("這是中性Bob");
+}
+*/
