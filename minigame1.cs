43b510c94416a5f6d82deb92cdefcaca00cb6d66
yrtay <yuanrentay365@gmail.com>
2023-03-20 11:53:16 +0800

    update minigame1

--- minigame1.cs
+++ minigame1.cs
@@ -5,20 +5,28 @@ int num = rand.Next(100);
 System.Console.WriteLine(num);
 */
 int rnum;
+int time = 5;
 do
 {
     System.Console.WriteLine("系統已經決定好了一個數字，你可以開始猜了（*整數100以內）");
-    rnum = System.Convert.ToInt32(System.Console.ReadLine());
-    if(rnum < num)
+    rnum = System.Convert.ToInt32(System.Console.ReadLine());time--;
+    if(time != 0)
     {
-        System.Console.WriteLine("大一點");
+        if(rnum < num)
+        {
+            System.Console.WriteLine("大一點");
+        }
+        else if(rnum > num)
+        {
+            System.Console.WriteLine("小一點");
+        }
     }
-    else if(rnum > num)
+    else
     {
-        System.Console.WriteLine("小一點");
+        System.Console.WriteLine("次數用完了，系統獲勝");
     }
 }
-while (rnum != num);
+while (rnum != num && time != 0);
 if(rnum == num)
 {
     System.Console.WriteLine("恭喜猜對了");
