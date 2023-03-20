d12882c95b395c0222aa1c27e18f03d4b2f8aabf
yrtay <yuanrentay365@gmail.com>
2023-03-20 11:04:00 +0800

    whileC#

---
+++ whileC#.cs
@@ -0,0 +1,16 @@
+﻿int num1 = 1;
+//直接判斷
+while (num1 <= 10)
+{
+    System.Console.WriteLine(num1);
+    num1++;
+}
+
+int  num2 = 9;
+//先執行，再判斷
+do
+{
+    System.Console.WriteLine(num2);
+    num2--;
+}
+while (num2 >= 1);
