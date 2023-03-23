fd7206ff9c65052ddc30db5c123be74d5c6edbd5
yrtay <yuanrentay365@gmail.com>
2023-03-23 12:17:04 +0800

    classC#

---
+++ Person(combine_to_classC#).cs
@@ -0,0 +1,25 @@
+﻿namespace Person{
+    class Head
+        {
+        public int eyes;
+        public int nose;
+        public int moouth;
+        public int ears;
+        }
+}
+/*
+ * 以此類推
+namespace A
+{
+    namespace a
+    {
+        public class aa
+        {
+            public aaa() { }
+        }
+    }
+}
+*/
+
+
+
---
+++ classC#.cs
@@ -0,0 +1,13 @@
+﻿using Person;
+/*
+ * 沒加using
+Person.Head head1 = new Head();
+*/
+Head head1 = new Head();
+
+head1.ears = 2;
+head1.eyes = 2;
+head1.nose = 1;
+head1.moouth = 1;
+
+System.Console.WriteLine(head1.ears);
