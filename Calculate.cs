71a3d399c7200c7a1f9ee10309e7029133cf26e0
yrtay <yuanrentay365@gmail.com>
2023-03-23 14:02:27 +0800

    calculator

---
+++ Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace calculator
+{
+    class Calculate
+    {
+        public double z;
+        public void add(double x, double y)
+        {
+            z = x + y;
+            Console.WriteLine("相加為:" + z);
+        }
+        public void sub(double x, double y)
+        {
+            z = x - y;
+            Console.WriteLine("相減為:" + z);
+        }
+        public void mul(double x, double y)
+        {
+            z = x * y;
+            Console.WriteLine("相乘為:" + z);
+        }
+        public void div(double x, double y)
+        {
+            z = x / y;
+            Console.WriteLine("相除為:" + z);
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Schema;
+using calculator;
+
+Console.WriteLine("請輸入兩個數字:");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+
+Calculate add1 = new Calculate();
+add1.add(a,b);
+add1.sub(a,b);
+add1.mul(a,b);
+add1.div(a,b);
